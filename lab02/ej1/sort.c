c8144382d08b8efcc0963d907a9f76586bfb703f
SantoII-github <SantoII1735@gmail.com>
2021-04-14 20:58:50 -0300

    lab 02 start

---
+++ lab02/ej1/sort.c
@@ -0,0 +1,24 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "array_helpers.h"
+#include "sort_helpers.h"
+#include "sort.h"
+
+
+static void insert(int a[], unsigned int i) {
+    unsigned int j = i;
+    while (j > 0 && a[j] < a[j - 1]) {
+        swap(a, j-1, j);
+        j = j - 1;
+    }
+}
+
+void insertion_sort(int a[], unsigned int length) {
+    for (unsigned int i = 1u; i < length; ++i) {
+        assert(array_is_sorted(a, i));              // Invariante: El segmento inicial a[0,i) del arreglo está ordenado. 
+        insert(a, i);
+    }
+}
