a15e58ec9d29ca1fcc3232d2f8642ece48daa1c2
SantoII-github <SantoII1735@gmail.com>
2021-04-15 00:35:47 -0300

    lab02 ej2

--- lab02/ej2/sort.c
+++ lab02/ej2/sort.c
@@ -9,12 +9,12 @@
 
 
 static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
- /* needs implementation */
-
-    /* no implementes partition, ya está implementado en sort_helpers.o
-       (no se puede leer, pero en sort_helpers.h vas a encontrar información
-        para saber cómo usarlo)
-    */
+    unsigned int ppiv;
+    if (der > izq) {
+        ppiv = partition(a, izq, der);
+        quick_sort_rec(a, izq, (ppiv == 0u) ? 0u : ppiv - 1u);
+        quick_sort_rec(a, ppiv+1, der);
+    }
 }
 
 void quick_sort(int a[], unsigned int length) {
