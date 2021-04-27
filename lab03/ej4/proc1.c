c01f0a5b4ec915b32c7961a97b3534c7e8ec5aab
SantoII-github <SantoII1735@gmail.com>
2021-04-27 16:15:26 -0300

    lab03 ej4

--- lab03/ej4/proc1.c
+++ lab03/ej4/proc1.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
 void absolute(int x, int y) {
-    // Completar aqui
+    if (x >= 0) {
+        y = x;
+    } else {
+        y = -x;
+    }
 }
 
 int main(void) {
-    // Completar aqui
+    int a, res;
+    res = 0;
+    a = -98;
+    absolute(a, res);
+    printf("%d", res);
     return 0;
 }
 
+/**
+ * El programa va a imprimir un 0 porque la variable y en absolute no mantiene su
+ * valor fuera de la función.
+ */
